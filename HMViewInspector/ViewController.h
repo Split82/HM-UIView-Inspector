d6749c27bc9f5851a8ed8e33e8f486eff6e776a2
Split <janilavsky@gmail.com>
2011-11-12 01:11:04 +0100

    Initial Commit

---
+++ HMViewInspector/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HMViewInspector
+//
+//  Created by Jan Ilavsky on 11/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
